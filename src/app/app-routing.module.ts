a00f984d6844f6d092703d38d25e2d6542a1b2d3
wwoi03 <tuandao8826848@gmail.com>
2024-07-12 21:52:02 +0700

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ecommerce',
+    loadChildren: () => import('./ecommerce/ecommerce.module').then(m => m.EcommerceModule),
+    //canActivate: [AuthGuard], // Bảo vệ các tuyến đường dưới 'admin'
+  },
+  // {
+  //   path: 'admin-public',
+  //   loadChildren: () => import('./admin-public/admin-public.module').then(m => m.AdminPublicModule),
+  // },
+  { path: '', redirectTo: 'admin/dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'admin/dashboard' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EcommerceComponent
   ],
   imports: [
     BrowserModule,
