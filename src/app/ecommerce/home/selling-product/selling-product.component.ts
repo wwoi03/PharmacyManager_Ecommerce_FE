93f5a8b8d72cfa6374acbd75bee657ce3d9fddb2
wwoi03 <tuandao8826848@gmail.com>
2024-07-14 16:25:53 +0700

    feat: done get selling product

--- src/app/ecommerce/home/selling-product/selling-product.component.ts
+++ src/app/ecommerce/home/selling-product/selling-product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ItemProductResponse } from '../../../models/responses/product/item-product-response';
+import { ProductService } from '../../../services/product/product.service';
 
 @Component({
   selector: 'ngx-selling-product',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './selling-product.component.scss'
 })
 export class SellingProductComponent {
+  itemProductResponses: ItemProductResponse[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.loadItemProduct();
+  }
+
+  loadItemProduct() {
+    this.productService.getSellingProductByMonth().subscribe(
+      (res) => {
+        if (res.code === 200) {
+          this.itemProductResponses = res.obj as ItemProductResponse[];
+        }
+      }
+    )
+  }
 }
--- src/app/models/responses/product/item-product-response.ts
+++ src/app/models/responses/product/item-product-response.ts
@@ -1,8 +1,8 @@
-export interface ItemProductResponse {
-    productId: string; 
-    productName: string;
-    specifications: string;
-    productImage: string;
-    pricePerUnit: { [key: string]: string };
-    discount: string;
+export class ItemProductResponse {
+  productId: string = '';
+  productName: string = '';
+  specifications: string = '';
+  productImage: string = '';
+  ShipmentDetailsUnitResponse: [] = [];
+  discount: number = 0;
 }
