2fc3ceda0265a37e4ba0af95bd20d12ddce43517
wwoi03 <tuandao8826848@gmail.com>
2024-07-21 00:54:40 +0700

    feat: add ngx-loading (account-sign in)

--- src/app/ecommerce/account/sign-in/sign-in.component.ts
+++ src/app/ecommerce/account/sign-in/sign-in.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { SignInRequest } from '../../../models/requests/account/sign-in-request';
 import { AccountService } from '../../../services/account/account.service';
 import { AuthService } from '../../../services/auth/auth.service';
+import { ngxLoadingAnimationTypes } from 'ngx-loading';
+
+const PrimaryWhite = '#ffffff';
+const SecondaryGrey = '#ccc';
+const PrimaryRed = '#dd0031';
+const SecondaryBlue = '#1976d2';
 
 @Component({
   selector: 'ngx-sign-in',
@@ -9,6 +15,21 @@ import { AuthService } from '../../../services/auth/auth.service';
   styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent {
+  emptyLoadingTemplate!: TemplateRef<any>;
+  showingTemplate = false;
+  public ngxLoadingAnimationTypes = ngxLoadingAnimationTypes;
+  public loading = false;
+  public primaryColour = PrimaryWhite;
+  public secondaryColour = SecondaryGrey;
+  public coloursEnabled = false;
+  public loadingTemplate!: TemplateRef<any>;
+  public config = {
+    animationType: ngxLoadingAnimationTypes.none,
+    primaryColour: this.primaryColour,
+    secondaryColour: this.secondaryColour,
+    tertiaryColour: this.primaryColour,
+    backdropBorderRadius: '3px',
+  };
   signInRequest: SignInRequest = new SignInRequest();
 
   constructor(
@@ -17,16 +38,19 @@ export class SignInComponent {
   ) {}
 
   signIn() {
+    this.loading = true;
+
     this.accountService.signIn(this.signInRequest).subscribe((res) => {
       if (res.code === 200) {
         this.authService.setToken(res.obj.token);
         this.authService.setName(res.obj.name);
+        console.log(this.authService.getToken());
         // this.toast.successToast("Thành Công", res.message);
 
-        // setTimeout(() => {
-        //   this.loading = false;
-        //   window.location.href = "/admin/dashboard";
-        // }, 1000);
+        setTimeout(() => {
+          this.loading = false;
+          //window.location.href = "/admin/dashboard";
+        }, 1000);
       } else if (res.code === 401) {
         // setTimeout(() => {
         //   this.loading = false;
