0884d353ed14bc42ec81a318dde3b10444525601
wwoi03 <tuandao8826848@gmail.com>
2024-07-30 19:33:49 +0700

    feat: done get list product

--- src/app/ecommerce/home/selling-product/selling-product.component.ts
+++ src/app/ecommerce/home/selling-product/selling-product.component.ts
@@ -1,28 +1,43 @@
 import { Component } from '@angular/core';
 import { ItemProductResponse } from '../../../models/responses/product/item-product-response';
 import { ProductService } from '../../../services/product/product.service';
+import { FileService } from 'src/app/services/file/file.service';
+import { UtilMoney } from 'src/app/helpers/utils/util-money';
+import { ShipmentDetailsUnitResponse } from 'src/app/models/responses/shipment-details-unit/shipment-details-unit-response';
 
 @Component({
   selector: 'ngx-selling-product',
   templateUrl: './selling-product.component.html',
-  styleUrls: ['./selling-product.component.scss']
+  styleUrls: ['./selling-product.component.scss'],
 })
 export class SellingProductComponent {
   itemProductResponses: ItemProductResponse[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    public fileService: FileService,
+    public utilMoney: UtilMoney,
+  ) {}
 
   ngOnInit() {
     this.loadItemProduct();
   }
 
   loadItemProduct() {
-    this.productService.getSellingProductByMonth().subscribe(
-      (res) => {
-        if (res.code === 200) {
-          this.itemProductResponses = res.obj as ItemProductResponse[];
-        }
+    this.productService.getSellingProductByMonth().subscribe((res) => {
+      if (res.code === 200) {
+        this.itemProductResponses = res.obj as ItemProductResponse[];
+
+        console.log(this.itemProductResponses);
       }
-    )
+    });
+  }
+
+  loadProductPrice(shipmentDetailsUnit: ShipmentDetailsUnitResponse[]) {
+    console.log(shipmentDetailsUnit[0]);
+  }
+
+  onClickBuy(item: ItemProductResponse) {
+    
   }
 }
--- src/app/models/responses/product/item-product-response.ts
+++ src/app/models/responses/product/item-product-response.ts
@@ -1,8 +1,11 @@
+import { ShipmentDetailsUnitResponse } from "../shipment-details-unit/shipment-details-unit-response";
+
 export class ItemProductResponse {
   productId: string = '';
   productName: string = '';
   specifications: string = '';
   productImage: string = '';
-  ShipmentDetailsUnitResponse: [] = [];
+  shipmentDetailsId: string = '';
+  shipmentDetailsUnits: ShipmentDetailsUnitResponse[] = [];
   discount: number = 0;
 }
